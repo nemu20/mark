163a25b128e431af9ba6ff1d883799aee4ba6430
nemu20 <eljohndoequeti@gmail.com>
2025-05-02 18:21:00 -0400

    while.py

--- while.py
+++ while.py
@@ -32,3 +32,68 @@
 #    else:
 #        print(f"el numero {num} es impar")
 
+# import random
+# numazar=random.randit(1,30)
+# print (numazar)
+# if numazar>=20:
+#     print("si ouede pasar")
+# else:
+#     print("le falta puntaje")
+
+
+# import random
+ 
+# numAzar=random.randint(1,50)
+ 
+# resnum=int(input("ingrese el numero para ver si adivninaste "))
+
+# while num!=numAzar:
+#     print(numAzar)
+#     if numAzar>resnum:
+#         print("el numero a adivinar es menor")
+#     else:
+#         print("el numero a adivinar es mayor")
+
+
+# import time
+# import random
+
+# meta=30
+# turno=1
+# j1=0
+# j2=0
+
+# while j1<meta and j2<meta:
+#     if turno%2==0:
+#         print("turno de j1")
+#         time.sleep(1)
+#         dado=random.randint(1,6)
+#         j1=j1+dado
+#         print("el j1 saco" , dado)
+# else:
+#     turno%2==0:
+#         print("turno de j2")
+#         time.sleep(1)
+#         dado=random.randint(1,6)
+#         j2=j2+dado
+#         print("el j2 saco", dado)
+    
+#     if j1==30:
+#         print("ha ganado el j1")
+#     else:
+#         print("ha ganado el j2")
+
+
+
+
+billetesde5=30
+billetesde10=30
+billetesde20=30
+
+claveusuario1=3426
+claveusuario2=2302
+claveusuario3=7942
+
+
+     
+    
