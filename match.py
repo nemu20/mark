a327929c6d1d19d3c92ce7bbe5329f7d4ac30eed
nemu20 <eljohndoequeti@gmail.com>
2025-05-13 16:22:36 -0400

    eljohndoequeti

---
+++ match.py
@@ -0,0 +1,22 @@
+
+def suma():
+    n1=int(input("ingrese un numero: :  "))
+    n2=int(input("ingrese otro numero: :  "))
+    print("el resultado de la suma es" , n1+n2)
+
+def resta():
+    n1=int(intu("ingres un numero: "))
+    n2=int(input("ingrese otro numero: "))
+    print("el resultado de la resta es" , n1-n2)
+
+def division():
+    n1=int(input("ingrese un numero: "))
+    n2=int(Input("ingrese otro numero: "))
+    print("el resultado de la division es" , n1/n2)
+    
+def multiplicacion():
+    n1=int(input("ingrese un numero: "))
+    n2=int(Input("ingrese otro numero: "))
+    print("el resultado de la multiplicacion es" , n1*n2)
+    
+    
---
+++ ty.py
@@ -0,0 +1,6 @@
+try:
+resutado= 10/0
+except ZeroDivisionError
+     print(f"se produjo una excepcion")
+else:
+print
